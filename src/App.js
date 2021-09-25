a4d11f1a4c8d9de3c5d37a1c6a63f38e6c207971
riyaverma338 <vermariya1202@gmail.com>
2021-09-25 23:44:43 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import './css/Weatherapp.css';
 import Weatherapp from './component/Weatherapp';
 const dotenv = require("dotenv");
-dotenv.config({path:'./config.env'});
-const API = process.env.API_KEY;
+dotenv.config({path: './config.env'});
+// dotenv.config({path:'./config.env'});
+// const API = process.env.API_KEY;
 
 function App() {
   return (
     <div>
+      
       <Weatherapp/>
     </div>
   );
--- src/component/Weatherapp.jsx
+++ src/component/Weatherapp.jsx
@@ -1,12 +1,14 @@
 import React, {useEffect,useState} from 'react';
 import '../css/Weatherapp.css';
 import location from '../location.gif.gif'
-export default function Weatherapp() {
+export default function Weatherapp( props) {
+
 
     const [city, setCity] = useState(null);   //hook
     const [search, setSearch] = useState("Mumbai"); //useEffect() hook tells react that your component needs to do something after render. React will remember the function you passed and call it later after performing the DOM updates.  
-
+    const API = process.env.API_KEY;
     useEffect( () => {
+        // console.log(props.API);
         const fetchApi = async () =>{
             const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&appid=43b7f97089111f4a01c6e892be4277e1`
             const response = await fetch(url); //fetch api returns promise
